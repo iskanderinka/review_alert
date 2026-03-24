e462f40938ed5e16075cfd2544a74e3a0e488589
Искандер Валеев <iskan_der@Iskanders-MacBook-Air.local>
2026-03-24 19:04:39 +0300

    v1.0.1

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 # Получаем список chat_id, разделенных запятыми, и преобразуем в список чисел
 CHAT_IDS = [int(chat_id.strip()) for chat_id in os.getenv('CHAT_IDS', '').split(',') if chat_id.strip()]
+PROXY_URL = os.getenv('PROXY_URL')
 SITE_URL = "https://turclub-pik.ru/comments/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
